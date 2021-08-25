6d074d3a44cdaf33dc723e27cd9ebc6aabddbbba
Jeonghum Joh <oosaprogrammer@gmail.com>
2021-08-25 15:20:57 +0900

    applied new step 1

--- tutorial.cxx
+++ tutorial.cxx
@@ -1,24 +1,24 @@
 
 // A simple program that computes the square root of a number
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+//#include <cstdlib.h>
 #include <math.h>
 #include "TutorialConfig.h"
 
 int main (int argc, char *argv[])
 {
-	if (argc < 2)
-	{
-		fprintf(stdout,"%s Version %d.%d\n",
-				argv[0],
-				Tutorial_VERSION_MAJOR,
-				Tutorial_VERSION_MINOR);
-		fprintf(stdout,"Usage: %s number\n",argv[0]);
-		return 1;
-	}
-	double inputValue = atof(argv[1]);
-	double outputValue = sqrt(inputValue);
-	fprintf(stdout,"The square root of %g is %g\n",
-			inputValue, outputValue);
-	return 0;
+  if (argc < 2) {
+    // report version
+    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+      << Tutorial_VERSION_MINOR << std::endl;
+    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    return 1;
+  }
+
+  const double inputValue = std::stod(argv[1]);
+  const double outputValue = sqrt(inputValue);
+
+  std::cout << "The square root of " << inputValue << " is " << outputValue << std::endl;
+
+  return 0;
 }
